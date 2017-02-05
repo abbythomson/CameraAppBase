ddb7e0f11c6412977e8dfea257deb820cd21bffe
abbythomson <abigailthomson13@augustana.edu>
2017-02-05 16:55:40 -0600

    editied

--- app/src/main/java/com/augustanasi/cameraex3/MainActivity.java
+++ app/src/main/java/com/augustanasi/cameraex3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.augustanasi.cameraex3;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +12,10 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 @SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
@@ -23,7 +28,9 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
     Camera.PictureCallback rawCallback;
     Camera.ShutterCallback shutterCallback;
     Camera.PictureCallback pngCallback;
+    Context context;
 
+    private String dir;
 
     private final String tag = "MainActivity";
 
@@ -32,10 +39,11 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //CHANGE TO GETCONTEXT IN FRAGMENT
+        context = getApplicationContext();
         start = (Button)findViewById(R.id.startbtn);
         stop = (Button)findViewById(R.id.stopbtn);
         take = (Button)findViewById(R.id.takebtn);
-        take.setVisibility(View.INVISIBLE);
 
         start.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View arg){
@@ -49,7 +57,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         });
         take.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
-              // captureImage();
+               captureImage();
             }
         });
 
@@ -71,20 +79,36 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
             }
         };
 
-       /* pngCallback = new Camera.PictureCallback() {
+        pngCallback = new Camera.PictureCallback() {
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
-                FileOutputStream outputStream = null;
+                FileOutputStream outputStream;
+
+                File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "AppPics");
+                File image = new File(storageDir, "image.png");
+
+
                 try{
-                    outputStream = new FileOutputStream(String.format("currentPhoto.png",System.currentTimeMillis()));
+                    outputStream = new FileOutputStream(image);
                     outputStream.write(data);
                     outputStream.close();
                     Log.d("Log", "onPictureTaken - wrote bytes: "+data.length);
+                } catch(FileNotFoundException e){
+                    e.printStackTrace();
+                } catch(IOException e){
+                    e.printStackTrace();
+                } finally {
                 }
+                Log.d("Log", "onPictureTaken - png");
             }
-        }*/
+        };
     }
 
+    private void captureImage(){
+        camera.takePicture(shutterCallback, rawCallback,pngCallback);
+        stopCamera();
+        startCamera();
+    }
     private void startCamera(){
         if(Camera.getNumberOfCameras()>0){
             if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT)){
@@ -100,7 +124,7 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         Camera.Parameters param;
         param = camera.getParameters();
 
-        param.setPreviewFrameRate(20);
+        param.setPreviewFrameRate(40);
         param.setPreviewSize(surfaceView.getWidth(),surfaceView.getHeight());
         camera.setParameters(param);
 
